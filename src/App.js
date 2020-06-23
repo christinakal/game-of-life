07df16fcb19381400bf0ad3184d20a1dbfec9559
Christina Kaliora <crhstina.kaliora@pirron.one>
2020-06-23 14:07:40 +0200

    grid is showing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="App">
       <h1>Game of Life</h1>
+      <Game />
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,43 @@
-// initialize grid
-let numRows = 25;
-let numColumns = 25;
+import React, { useState } from "react";
 
-// Create grid
-const resetGrid = () => {
+const numRows = 50;
+const numCols = 50;
+
+const Game = () => {
+  const [grid, setGrid] = useState(() => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
-        rows.push(Array.from(Array(numColumns), () => 0))
+      //initializing sells as 0 because they're all gonna be dead by default
+      rows.push(Array.from(Array(numCols), () => 0));
     }
+
     return rows;
-}
+  });
 
+  console.log(grid);
 
-return (
-    <div></div>
-)
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      }}
+    >
+      {grid.map((rows, rowIndex) =>
+        rows.map((col, colIndex) => (
+          <div
+            key={`${rowIndex} - ${colIndex}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[rowIndex][colIndex] ? "pink" : undefined,
+              border: "solid 1px black",
+            }}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Game;
