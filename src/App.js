45e7ea85e97746f4e2e46974068d9c11c599de8c
christina kaliora <develop@pirron.one>
2020-06-25 10:14:28 +0200

    finished the about page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Game from "./components/Game";
+import Rules from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <h1>Game of Life</h1>
+      <Rules />
       <Game />
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Rules = () => {
+  return (
+    <div className="rules">
+      <div className="about">
+        <h1>About Game</h1>
+        <p>
+          The Game of Life is a cellular automaton invented in 1970 by British
+          mathematician, John Conway. It is a zero-player game, meaning that its
+          evolution is determined by its initial state. It consists of a grid of
+          cells that can live, die or multiply based on a set of mathematical
+          rules. For more info go{" "}
+          <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">
+            here!
+          </a>
+        </p>
+      </div>
+      <h1>Rules</h1>
+      <div>
+        Any live cell with fewer than two lives the neighbours die, as if by
+        underpopulation.
+      </div>
+      <div>
+        Any live cell with two or three live neighbours lives on to the next
+        generation.
+      </div>
+      <div>
+        Any live cell with more than three live neighbours dies, as if by
+        overpopulation.
+      </div>
+      <div>
+        Any dead cell with exactly three live neighbours becomes a live cell, as
+        if by reproduction.
+      </div>
+    </div>
+  );
+};
+export default Rules;
